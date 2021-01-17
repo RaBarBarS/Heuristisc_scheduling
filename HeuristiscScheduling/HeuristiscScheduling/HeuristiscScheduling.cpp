de0f3b742436937fb5f47967c5756ebcb0f120c9
RaBarBarS <bacha.samsel@gmail.com>
2021-01-17 17:24:55 +0100

    debugged, almost working

--- HeuristiscScheduling/HeuristiscScheduling/HeuristiscScheduling.cpp
+++ HeuristiscScheduling/HeuristiscScheduling/HeuristiscScheduling.cpp
@@ -15,11 +15,11 @@
 
 //params
 const double workingTime = 20;	//time in secs
-const int iterationsNumber = 10;   //number of iterations to run
+const int iterationsNumber = 3;   //number of iterations to run
 bool iterations = true; // set app to run given iterations number if true, if false app run given perioid of time
-const int populationSize = 180; 
-const int parentsForGeneration = 10;    //number of parents to be selected fo next generation
-std::string input = "test_3_0.txt";
+const int populationSize = 10; 
+const int parentsForGeneration = 5;    //number of parents to be selected fo next generation
+std::string input = "test_4_0.txt";
 //given problem
 int problemSize = 10;///przerób tab poniżej na dynamiczny
 std::vector<std::vector<int>> problem;    //tasks times
@@ -83,7 +83,7 @@ void roulette() {
     }
 
     for (int i = 0; i < populationSize; i++) {
-        probabilityOfParent[i] = (1 - (populationScores[i] / sumOfScores)) / sumOfScores;   //as we want the smallest score to have the biggest probability we have to substract from 1...
+        probabilityOfParent[i] = (1 - (populationScores[i] / sumOfScores));   //as we want the smallest score to have the biggest probability we have to substract from 1...
         probabilitySum += probabilityOfParent[i];   //probabilitest created like that won't sum to 1, so we have to sum them
     }
 
@@ -176,10 +176,14 @@ void nextGen() {//taking task order form parents    ///spr numerki od zadań, pr
             }
             solutions[i].push_back(newSolution);
         }
+        solutions[i].erase(solutions[i].begin(), solutions[i].begin() + 2);
     }
 }
 
 void addMutations() {
+    if (problemSize < 5) {
+        return; //nie ma sensu na tak małym rozwiązaniu robić mutacji
+    }
     for (int i = 0; i < populationSize; i++) {
         for (int j = 0; j < 2; j++) {
             int mutationNumber = rand() % (problemSize / 5);	//random number of mutations but no more than 20% of solution size
@@ -203,8 +207,9 @@ void applySchedulingRulesM1() {
         for (int i = 0; i < problemSize; i++) {
             if (timeToBreakM1 - problem[0][solutions[s][0][i]] < 0) {
                 helper.push_back(mainten);
+                timeToBreakM1 = maxTimeBetweenBreaks1;
             }
-            timeToBreakM1 = maxTimeBetweenBreaks1 - problem[0][solutions[s][0][i]];
+            timeToBreakM1 = timeToBreakM1 - problem[0][solutions[s][0][i]];
             helper.push_back(solutions[s][0][i]);
         }
         solutions[s][0].clear();
@@ -220,8 +225,13 @@ void applySchedulingRulesM2() {
     for (int s = 0; s < populationSize; s++) {
         int time1m = 0, time2m = 0, timeToBreakM2 = maxTimeBetweenBreaks2;
         std::vector<int> readyTasksForM2;
+        std::vector<int> endedonM1;//czasy zakończeń zadań na M1
         bool isThatAll = false;
         
+        for (int i = 0; i < problemSize; i++) {
+            endedonM1.push_back(0);
+        }
+
         std::vector<int> helper;//przechowuje M2, bo na razie tylko solution dla M1 jest zapisane i później się skopiuje vector do solM2
         for (int i = 0, j = 0; i < solutions[s][0].size(); i++, j++) {
             //dodanie do czasu M1 kolejnej pozycji
@@ -230,6 +240,7 @@ void applySchedulingRulesM2() {
             }
             else if (solutions[s][0][i] >= 0 && solutions[s][0][i] < mainten) {
                 time1m += problem[0][solutions[s][0][i]];
+                endedonM1[solutions[s][0][i]] = time1m;//przypisz czas zakończenia zadania na M1
                 readyTasksForM2.push_back(solutions[s][0][i]);
             }
             else {
@@ -277,20 +288,37 @@ void applySchedulingRulesM2() {
                 if (time1m > time2m) {
                     std::sort(readyTasksForM2.begin(), readyTasksForM2.end());//posortowane rosnąco
                     for (int t = readyTasksForM2.size() - 1; t >= 0; t--) {
-                        if (problem[1][readyTasksForM2[t]] <= timeToBreakM2) {
-                            helper.push_back(readyTasksForM2[t]);
-                            time2m += problem[1][readyTasksForM2[t]];
-                            timeToBreakM2 -= problem[1][readyTasksForM2[t]];
-                            readyTasksForM2.pop_back();//usuń element, bo juz ododany
-                        }
                         if (readyTasksForM2.size() && timeToBreakM2 < problem[1][readyTasksForM2[0]]) {//jesi coś jeszcze zostało, ale nawet najmniejszy się już nie zmiesci
                             if ((timeToBreakM2 - breakLen2) % breakLen2 > 0) {//jeśli idle > 0
                                 helper.push_back(((timeToBreakM2 - breakLen2) % breakLen2) * -1);//dodaj idle an początku wielkości reszty z dzielenia przez dł przerwy
                                 time2m += timeToBreakM2 - breakLen2;//dadaj czas idle
                                 timeToBreakM2 -= timeToBreakM2 - breakLen2;
                             }
+                            helper.push_back(mainten);
+                            time2m += breakLen2;
+                            timeToBreakM2 = maxTimeBetweenBreaks2;
+                        }
+                        if (problem[1][readyTasksForM2[t]] <= timeToBreakM2 && time2m >= endedonM1[readyTasksForM2[t]]) {//jeśli problem zmieści się przed przerwą i skończył się na M1
+                            helper.push_back(readyTasksForM2[t]);
+                            time2m += problem[1][readyTasksForM2[t]];
+                            timeToBreakM2 -= problem[1][readyTasksForM2[t]];
+                            readyTasksForM2.pop_back();//usuń element, bo juz ododany
+                        }
+                        else if (readyTasksForM2.size() && timeToBreakM2 >= problem[1][readyTasksForM2[0]]){//jeśłi coś zostało, zmieści się do końca przerwy, ale nie zostało
+                            //wstawione to znaczy, że nei skończyło się na M1 i trzeba M2 uzupełnić idlami i przerwami
+                            int smallestEndedTime = time1m;//jak przypiszę czas cąłej maszyny to cos powinno byc mniejsze
+                            for (int n = 0; n < problemSize; n++) {
+                                if (endedonM1[n] > time2m && smallestEndedTime > endedonM1[n]) {
+                                    smallestEndedTime = endedonM1[n];
+                                }
+                            }
+                            if ((smallestEndedTime - time2m) % breakLen2 > 0) {//jeśli idle > 0
+                                helper.push_back(((smallestEndedTime - time2m) % breakLen2) * -1);//dodaj idle an początku wielkości reszty z dzielenia przez dł przerwy
+                                timeToBreakM2 -= (smallestEndedTime - time2m) % breakLen2;
+                                time2m += (smallestEndedTime - time2m) % breakLen2;//dadaj czas idle
+                            }
                             //resztę uzupełnij przerwami jeśli trzeba
-                            while (timeToBreakM2 != maxTimeBetweenBreaks2) {
+                            while (smallestEndedTime != time2m) {
                                 helper.push_back(mainten);
                                 time2m += breakLen2;
                                 timeToBreakM2 = maxTimeBetweenBreaks2;
@@ -324,49 +352,97 @@ void applySchedulingRulesM2() {
                     timeToBreakM2 -= time1m - time2m;//odejmin czas idle od czasu do wymaganej konserwacji
                 }
             }
-            //NA 99% NEI POTRZEBNE
-            //if (i == solutions[s][0].size() - 1) {//jeśli to ostatnia iteracja
-            //    for (int h = 0; h < problemSize; h++) {
-            //        if (tab[h] == 0) {
-            //            isThatAll = false;
-            //            break;
-            //        }
-            //        isThatAll = true;
-            //    }
-            //    while (!isThatAll) {
-            //        int first = solutions[s][1].size();
-            //        for (int d = 0, m = 0; d < problemSize && m < solutions[s][0].size(); d++, m++) {	//dwie zmienne żeby pomijać przerwy
-            //            if (solutions[s][0][m] == maintenance) {
-            //                d--;
-            //            }
-            //            else {
-            //                if (tab[solutions[s][0][m]] == 0) {
-            //                    for (int c = 0; c < solutions[s][1].size(); c++) {
-            //                        if (solutions[s][1][c] == solutions[s][0][m]) {
-            //                            if (first > c)
-            //                                first = c;
-            //                            break;//bo tylko jedno wystąpienie w całym rozwiązaniu wiec po co tracić czas
-            //                        }
-            //                    }
-            //                }
-            //            }
-            //        }
-            //        helper.push_back(solutions[s][1][first]);
-            //        tab[solutions[s][1][first]] = true;
-            //        time2m += problem[1][solutions[s][1][first]];
-            //        for (int h = 0; h < problemSize; h++) {
-            //            if (tab[h] == 0) {
-            //                isThatAll = false;
-            //                break;
-            //            }
-            //            isThatAll = true;
-            //        }
-            //    }
-            //}
+            if (i == solutions[s][0].size() - 1) {//jeśli to ostatnia iteracja
+                if (readyTasksForM2.size()) {
+                    isThatAll = false;  //jeśli coś jeszcze do uzupełnienia to ustawisThatAll na false
+                }
+                else {
+                    isThatAll = true;
+                }
+                while (!isThatAll) {
+                    for (int t = readyTasksForM2.size() - 1; t >= 0; t--) {
+                        if (readyTasksForM2.size() && timeToBreakM2 < problem[1][readyTasksForM2[0]]) {//jesi coś jeszcze zostało, ale nawet najmniejszy się już nie zmiesci
+                            if ((timeToBreakM2 - breakLen2) % breakLen2 > 0) {//jeśli idle > 0
+                                helper.push_back(((timeToBreakM2 - breakLen2) % breakLen2) * -1);//dodaj idle an początku wielkości reszty z dzielenia przez dł przerwy
+                                time2m += timeToBreakM2 - breakLen2;//dadaj czas idle
+                                timeToBreakM2 -= timeToBreakM2 - breakLen2;
+                            }
+                            helper.push_back(mainten);
+                            time2m += breakLen2;
+                            timeToBreakM2 = maxTimeBetweenBreaks2;
+                        }
+                        if (problem[1][readyTasksForM2[t]] <= timeToBreakM2 && time2m >= endedonM1[readyTasksForM2[t]]) {//jeśli problem zmieści się przed przerwą i skończył się na M1
+                            helper.push_back(readyTasksForM2[t]);
+                            time2m += problem[1][readyTasksForM2[t]];
+                            timeToBreakM2 -= problem[1][readyTasksForM2[t]];
+                            readyTasksForM2.pop_back();//usuń element, bo juz ododany
+                            if (readyTasksForM2.size()) {
+                                isThatAll = false;  //jeśli coś jeszcze do uzupełnienia to ustawisThatAll na false
+                            }
+                            else {
+                                isThatAll = true;
+                            }
+                        }
+                        else if (readyTasksForM2.size() && timeToBreakM2 >= problem[1][readyTasksForM2[0]]) {//jeśłi coś zostało, zmieści się do końca przerwy, ale nie zostało
+                            //wstawione to znaczy, że nei skończyło się na M1 i trzeba M2 uzupełnić idlami i przerwami
+                            int smallestEndedTime = time1m;//jak przypiszę czas cąłej maszyny to cos powinno byc mniejsze
+                            for (int n = 0; n < problemSize; n++) {
+                                if (endedonM1[n] > time2m && smallestEndedTime > endedonM1[n]) {
+                                    smallestEndedTime = endedonM1[n];
+                                }
+                            }
+                            if ((smallestEndedTime - time2m) % breakLen2 > 0) {//jeśli idle > 0
+                                helper.push_back(((smallestEndedTime - time2m) % breakLen2) * -1);//dodaj idle an początku wielkości reszty z dzielenia przez dł przerwy
+                                timeToBreakM2 -= (smallestEndedTime - time2m) % breakLen2;
+                                time2m += (smallestEndedTime - time2m) % breakLen2;//dadaj czas idle
+                            }
+                            //resztę uzupełnij przerwami jeśli trzeba
+                            while (smallestEndedTime != time2m) {
+                                helper.push_back(mainten);
+                                time2m += breakLen2;
+                                timeToBreakM2 = maxTimeBetweenBreaks2;
+                            }
+                        }
+                    }
+                }
+            }
         }
         solutions[s][1].clear();
         solutions[s].erase(solutions[s].begin() + 1);//usuwa M2
         solutions[s].push_back(helper);//dodaje nowe M2
+    }
+}
+
+void printSolutions() {
+    for (int i = 0; i < populationSize; i++) {
+        std::cout << "solution: " << i << '\n' << "m1: ";
+
+        for (int j = 0; j < solutions[i][0].size(); j++) {
+            if (solutions[i][0][j] == 30000) {
+                std::cout << "m" << breakLen1 << " ";
+            }
+            else if (solutions[i][0][j] >= 0) {
+                std::cout << solutions[i][0][j] << ": " << problem[0][solutions[i][0][j]]<< " ";
+            }
+            else {
+                std::cout << "idle" << solutions[i][0][j] * -1 << " ";
+            }
+        }
+
+        std::cout << '\n' << "m2: ";
+
+        for (int j = 0; j < solutions[i][1].size(); j++) {
+            if (solutions[i][1][j] == 30000) {
+                std::cout << "m" << breakLen2 << " ";
+            }
+            else if (solutions[i][1][j] >= 0) {
+                std::cout << solutions[i][1][j] << ": " << problem[1][solutions[i][1][j]] << " ";
+            }
+            else {
+                std::cout << "idle" << solutions[i][1][j] * -1 << " ";
+            }
+        }
+        std::cout << '\n';
     }
 }
 
@@ -447,7 +523,7 @@ void saveToFile() {
         if (solutions[theBestSolution][0][i] == 30000) {
             file << "m" << breakLen1 << " ";
         }
-        else if (solutions[theBestSolution][0][i] > 0) {
+        else if (solutions[theBestSolution][0][i] >= 0) {
             file << solutions[theBestSolution][0][i] << " ";
         }
         else {
@@ -460,7 +536,7 @@ void saveToFile() {
         if (solutions[theBestSolution][0][i] == 30000) {
             file << "m" << breakLen2 << " ";
         }
-        else if (solutions[theBestSolution][0][i] > 0) {
+        else if (solutions[theBestSolution][0][i] >= 0) {
             file << solutions[theBestSolution][0][i] << " ";
         }
         else {
@@ -481,15 +557,22 @@ int main()
             populate();
             applySchedulingRulesM1();
             applySchedulingRulesM2();
+            printSolutions();
             for (int i = 0; i < populationSize; i++) {
                 populationScores[i] = countCmax(i);
             }
+            /// <summary>
+            /// DODAJ WYBIERANIE NAJLEPSZEGO
+            /// </summary>
+            /// <returns></returns>
             while (timeIsOver < iterationsNumber) {
                 roulette();
                 nextGen();
+                printSolutions();
                 addMutations();
                 applySchedulingRulesM1();
                 applySchedulingRulesM2();
+                printSolutions();
                 for (int i = 0; i < populationSize; i++) {
                     populationScores[i] = countCmax(i);
                 }
